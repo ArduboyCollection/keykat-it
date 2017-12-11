f981d616b22091f9016558790e2bb5f55277c13f
Robert Lowe <robert.lowe@maryvillecollege.edu>
2017-12-11 17:57:05 -0500

    A little animation test and proper sprite storage

--- keykat-it.ino
+++ keykat-it.ino
@@ -1,17 +1,54 @@
 #include <Arduboy.h>
+#include "assets.h"
 Arduboy arduboy;
 
-const unsigned char keykat[] PROGMEM = {
-  0x80, 0x8c, 0x9f, 0xfe, 0xfe, 0x9f, 0x8c, 0x80, 0x1, 0xf1, 0xf9, 0x1f, 0x1f, 0xf9, 0xf1, 0x1
-};
+unsigned int frameCount = 0;
+
+void renderSprite(int x, int y, sprite &s);
+
+sprite alist[5];
 
 void setup() {
   arduboy.begin();
   arduboy.clear();
+  arduboy.setFrameRate(60);
+  alist[0] = keykat_standing;
+  alist[1] = keykat_walking;
+  alist[2] = computer_off;
+  alist[3] = computer_working;
+  alist[4] = computer_broken;
 }
 
+
 void loop() {
- arduboy.clear(); 
- arduboy.drawBitmap(5, 10, keykat, 8, 16, WHITE); 
- arduboy.display();
+  //wait until the next frame
+  if(not arduboy.nextFrame()) return;
+
+  //update the frame counter
+  ++frameCount;
+  if(frameCount > 60) {
+    frameCount -= 60;
+  }
+
+  arduboy.clear(); 
+  int x = 8;
+
+  for(int i=0; i<5; i++) {
+    renderSprite(x, 5, alist[i]);
+    x+=16;
+  }
+  
+  arduboy.display();
 }
+
+
+void renderSprite(int x, int y, sprite &s)
+{
+  if(frameCount % s.fdelay == 0) {
+    s.fi = (s.fi+1) % s.frames;
+  }
+  
+  //display the bitmap
+  arduboy.drawBitmap(x, y, sprite_data + s.offset + s.fi*s.height, s.width, s.height, WHITE); 
+}
+
